f9324da282456f8a1b8389bc8d69889cb375c778
NexusLAST <powarts2@gmail.com>
2025-12-31 00:11:39 +0100

    Add files via upload

---
+++ pages/EventMap.tsx
@@ -0,0 +1,10 @@
+import { Layout } from "@/Layout/Layout";
+import { EventMapDetailed } from "@/Events/EventMapDetailed";
+
+export default function EventMap() {
+  return (
+    <Layout>
+      <EventMapDetailed />
+    </Layout>
+  );
+}
